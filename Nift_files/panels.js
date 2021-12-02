abf393698fc4cc8a532a2f283270c17b4fc53664
TheNift <95388564+TheNift@users.noreply.github.com>
2021-12-01 21:13:35 -0600

    Deleted some comments

--- Nift_files/panels.js
+++ Nift_files/panels.js
@@ -3,13 +3,6 @@ const info_panels = document.querySelectorAll('.info-panel');
 
 for (let button of info_buttons) {
     button.addEventListener('click', ()=> {
-        // let shownPanel = document.querySelector('.info-panel.shown');
-        // if (shownPanel) {
-        //     shownPanel.classList.remove('shown');
-        // } else {
-        //     // document.querySelector('.info-panel').classList.add('shown');
-        //     // info_panels[0].classList.add('shown');
-        // }
         for(let i = 0; i < info_panels.length; i++) {
             if(info_panels[i].id == button.id) {
                 if(info_panels[i].classList.contains('shown')) {
