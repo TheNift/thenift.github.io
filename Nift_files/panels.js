db16cf1ad6d556e4b854e56b46b60a643fbc8a22
TheNift <95388564+TheNift@users.noreply.github.com>
2022-01-01 22:11:43 -0600

    testing adding scrollbar to projects page

--- Nift_files/panels.js
+++ Nift_files/panels.js
@@ -15,6 +15,13 @@ for (let button of info_buttons) {
                     info_panels[i].classList.remove('shown');
                 }
             }
+            if(info_panels[i].id == "projects") {
+                if(info_panels[i].classList.contains('shown')) {
+                    document.body.style.overflow = visible;
+                } else {
+                    document.body.style.overflow = hidden;
+                }
+            }
         }
     });
 }
