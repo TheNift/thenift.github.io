2c88d0e59639bdcc36acacc696249dfe4d3a9346
Jack <jackkillschool@gmail.com>
2023-07-18 11:22:46 -0400

    Fixed grid

--- Nift_files/tiles.js
+++ Nift_files/tiles.js
@@ -44,10 +44,10 @@ const createTiles = quantity => {
 const createGrid = () => {
   wrapper.innerHTML = "";
   
-  const size = document.body.clientWidth > 800 ? 100 : 100;
+  const size = document.body.clientWidth > 800 ? 100 : 50;
   
   columns = Math.floor(document.body.clientWidth / size);
-  rows = Math.floor(document.body.clientHeight / size);
+  rows = Math.floor(document.body.clientHeight / size) * 2;
   
   wrapper.style.setProperty("--columns", columns);
   wrapper.style.setProperty("--rows", rows);
